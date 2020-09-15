fb7c8323c5230791e9677fbdac7de33dbd9dc187
cuncis <cuncis1st@gmail.com>
2020-09-15 12:56:22 +0700

    add feature upload data

--- app/src/main/java/com/cuncisboss/firestoresample1/MainActivity.kt
+++ app/src/main/java/com/cuncisboss/firestoresample1/MainActivity.kt
@@ -9,59 +9,43 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val REQUEST_CODE_SIGN_IN = 0
-    }
 
-    private lateinit var auth: FirebaseAuth
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val personCollectionPref = Firebase.firestore.collection("persons")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
 
-        btnGoogleSignIn.setOnClickListener {
-            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.webclient_id))
-                .requestEmail()
-                .build()
-            val signInClient = GoogleSignIn.getClient(this, options)
-            signInClient.signInIntent.also {
-                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
-            }
+        btnUploadData.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
         }
     }
 
-    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
-        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                auth.signInWithCredential(credentials)
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(this@MainActivity, "Successfully logged in", Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-                }
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionPref.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully added data", Toast.LENGTH_SHORT).show()
             }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_SIGN_IN) {
-            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
-            account?.let {
-                googleAuthForFirebase(account)
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
