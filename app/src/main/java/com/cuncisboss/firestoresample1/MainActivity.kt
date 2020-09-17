7274852d7ee7da548dbb134ce8fdc92b034b48db
cuncis <cuncis1st@gmail.com>
2020-09-17 08:48:06 +0700

    add feature uploading image

--- app/src/main/java/com/cuncisboss/firestoresample1/MainActivity.kt
+++ app/src/main/java/com/cuncisboss/firestoresample1/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.cuncisboss.firestoresample1
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -10,6 +13,7 @@ import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -18,198 +22,38 @@ import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val REQUEST_CODE_IMAGE_PICK = 1
+    }
 
-    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
-    private val personCollectionPref = Firebase.firestore.collection("persons")
+    private var curUri: Uri? = null
+
+    private val imageRef = Firebase.storage.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnUploadData.setOnClickListener {
-            val person = getOldPerson()
-            savePerson(person)
-        }
-
-        btnRetrieveData.setOnClickListener {
-            retrievePerson()
-        }
-
-        btnDeletePerson.setOnClickListener {
-            val person = getOldPerson()
-            deletePerson(person)
-        }
-
-        btnUpdatePerson.setOnClickListener {
-            val oldPerson = getOldPerson()
-            val newPerson = getNewPersonMap()
-            updatePerson(oldPerson, newPerson)
-        }
-
-        btnBatchedWrite.setOnClickListener {
-            changeName("OSgr1JWudeKP9NN6tLvE", "Viga", "Cha")
-        }
-
-        btnTransaction.setOnClickListener {
-            birthday("OSgr1JWudeKP9NN6tLvE")
-        }
-    }
-
-    private fun getOldPerson(): Person {
-        val firstName = etFirstName.text.toString()
-        val lastName = etLastName.text.toString()
-        val age = etAge.text.toString().toInt()
-        return Person(firstName, lastName, age)
-    }
-
-    private fun getNewPersonMap(): Map<String, Any> {
-        val firstName = etNewFirstName.text.toString()
-        val lastName = etNewLastName.text.toString()
-        val age = etNewAge.text.toString()
-        val map = mutableMapOf<String, Any>()
-        if (firstName.isNotEmpty()) {
-            map["firstName"] = firstName
-        }
-        if (lastName.isNotEmpty()) {
-            map["lastName"] = lastName
-        }
-        if (age.isNotEmpty()) {
-            map["age"] = age.toInt()
-        }
-        return map
-    }
-
-    private fun birthday(personId: String) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            Firebase.firestore.runTransaction { transaction ->
-                val personRef = personCollectionPref.document(personId)
-                val person = transaction.get(personRef)
-                val newAge = person["age"] as Long + 1
-                transaction.update(personRef, "age", newAge)
-            }.await()
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+        ivImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                it.type = "image/*"
+                startActivityForResult(it, REQUEST_CODE_IMAGE_PICK)
             }
         }
-    }
 
-    private fun changeName(
-        personId: String,
-        newFirstName: String,
-        newLastName: String
-    ) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            Firebase.firestore.runBatch { writeBatch ->
-                val personRef = personCollectionPref.document(personId)
-                writeBatch.update(personRef, "firstName", newFirstName)
-                writeBatch.update(personRef, "lastName", newLastName)
-                writeBatch.update(personRef, mapOf(
-                    "address" to FieldValue.delete()
-                ))
-            }.await()
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-            }
+        btnUploadImage.setOnClickListener {
+            uploadImageToStorage("myImage")
         }
     }
 
-    private fun deletePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
-        val personQuery = personCollectionPref
-            .whereEqualTo("firstName", person.firstName)
-            .whereEqualTo("lastName", person.lastName)
-            .whereEqualTo("age", person.age.toInt())
-            .get()
-            .await()
-
-        if (personQuery.documents.isNotEmpty()) {
-            for (document in personQuery) {
-                try {
-                    personCollectionPref.document(document.id).delete().await()
-//                    personCollectionPref.document(document.id).update(mapOf(
-//                        "firstName" to FieldValue.delete()
-//                    ))
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-                    }
+    private fun uploadImageToStorage(filename: String) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            curUri?.let {
+                imageRef.child("images/$filename").putFile(it).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Successfully uploaded image", Toast.LENGTH_SHORT).show()
                 }
             }
-        } else {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, "No person matched the query", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun updatePerson(person: Person, newPersonMap: Map<String, Any>) = CoroutineScope(Dispatchers.IO).launch {
-        val personQuery = personCollectionPref
-            .whereEqualTo("firstName", person.firstName)
-            .whereEqualTo("lastName", person.lastName)
-            .whereEqualTo("age", person.age.toInt())
-            .get()
-            .await()
-
-        if (personQuery.documents.isNotEmpty()) {
-            for (document in personQuery) {
-                try {
-                    personCollectionPref.document(document.id).set(
-                        newPersonMap,
-                        SetOptions.merge()
-                    )
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        } else {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, "No person matched the query", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun subscribeToRealtimeDatabase() {
-        personCollectionPref
-//            .whereEqualTo("firstName", "peter")
-            .addSnapshotListener { value, error ->
-            value?.let {
-                val sb = StringBuilder()
-                for (document in it) {
-                    val person = document.toObject<Person>()
-                    sb.append("$person\n")
-                }
-
-                tvPersons.text = sb.toString()
-            }
-            error?.let {
-                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
-                return@addSnapshotListener
-            }
-        }
-    }
-
-    private fun retrievePerson() = CoroutineScope(Dispatchers.IO).launch {
-        val fromAge = etFrom.text.toString().toInt()
-        val toAge = etTo.text.toString().toInt()
-        try {
-            val querySnapshot = personCollectionPref
-                .whereGreaterThan("age", fromAge)
-                .whereLessThanOrEqualTo("age", toAge)
-                .orderBy("age")
-                .get()
-                .await()
-            val sb = StringBuilder()
-            for (document in querySnapshot) {
-                val person = document.toObject<Person>()
-                sb.append("$person\n")
-            }
-            Log.d("_logFirebase", "retrievePerson: $sb")
-            withContext(Dispatchers.Main) {
-                tvPersons.text = sb.toString()
-            }
         } catch (e: Exception) {
             withContext(Dispatchers.Main) {
                 Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
@@ -217,18 +61,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
-        try {
-            personCollectionPref.add(person).await()
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, "Successfully added data", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_IMAGE_PICK) {
+            data?.data?.let {
+                curUri = it
+                ivImage.setImageURI(it)
             }
         }
     }
+
+
 }
 
 
